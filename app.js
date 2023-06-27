f67a5b7cd0f91625b27ec15ffd5b2f62035dd881
Donald Soh <sohdonald@gmail.com>
2023-06-27 11:01:05 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+/* my notes here
+
+practice .target and Object types here
+
+db.json has Zombie hamsters and heroes to fight them
+
+
+can we use e.target to decrease the zombieHp every
+time a hero attacks a zombie?
+
+how can we use Object. to make the heroes and zombies appear in the browser?
+
+make a function that will increase the heroExp
+when the heroExp reaches a certain number, it increases the hero's level to ++1
+
+make a button that allows the heroes to attack a zombie
+when clicked, a text message asks "which zombie"
+and the zombie hamsters become clickable
+the zombie clicked loses certain amount of hp
+
+e.target
+
+some of the heroes have spells that can attack multiple zombies
+
+this should be a good opportunity to practice fetch
+
+all the Object. are static methods,
+What is a static method?
+
+*/
+
+// code here
